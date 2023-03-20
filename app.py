648bb9cec18c951b2de3206c5706e5c852c337a5
Nitin Kumar <nitinking316@gmail.com>
2023-03-20 20:49:12 +0530

    flask app

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('home.html') 
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
